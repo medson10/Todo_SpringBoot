b4322ea7935e8caac5dd0ad20e86159e47027255
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-04-11 14:59:38 -0300

    Class Team added

---
+++ src/main/java/teams/Team.java
@@ -0,0 +1,46 @@
+package java;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.sun.java.util.jar.pack.Package.Class.Member;
+
+@Entity
+public class Team implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL)
+    private List<Member> members;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+	
+}
