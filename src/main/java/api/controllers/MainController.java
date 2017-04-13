87e8102d930fe4f683ff41246828b0c3d4b5e065
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-04-13 15:20:02 -0300

    Add CrossOrigin

--- src/main/java/api/controllers/MainController.java
+++ src/main/java/api/controllers/MainController.java
@@ -2,8 +2,11 @@ package api.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RestController
+@CrossOrigin
 public class MainController {
 
 
