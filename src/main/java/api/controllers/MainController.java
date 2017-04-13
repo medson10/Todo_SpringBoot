94868b4c3f0b86fad6f75b0b07560f1c06fecf60
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-04-13 02:03:28 -0300

    Add cors

--- src/main/java/api/controllers/MainController.java
+++ src/main/java/api/controllers/MainController.java
@@ -3,11 +3,11 @@ package api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(options = "*" maxAge = 3600)
 @RestController
 public class MainController {
 
-    @CrossOrigin
+
     @RequestMapping("/")
     public String index() {
         return "Hello World";
