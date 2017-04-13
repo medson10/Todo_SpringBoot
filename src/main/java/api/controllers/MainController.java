6f2dd5b552c8ed54c08cafb2dfe5dfa035f497f4
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-04-13 02:34:23 -0300

    Remove Cors

--- src/main/java/api/controllers/MainController.java
+++ src/main/java/api/controllers/MainController.java
@@ -3,7 +3,6 @@ package api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(options = "*" maxAge = 3600)
 @RestController
 public class MainController {
 
