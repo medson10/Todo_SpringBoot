a5edd979b82355aea2baa700795b776fa02379e8
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-04-17 14:34:26 -0300

    Cors filter updated

--- src/main/java/api/SimpleCorsFilter.java
+++ src/main/java/api/SimpleCorsFilter.java
@@ -15,19 +15,16 @@ import org.springframework.stereotype.Component;
 public class SimpleCorsFilter implements Filter {
   public SimpleCorsFilter() {}
 
-  @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-       HttpServletResponse response = (HttpServletResponse) res;
-       response.setHeader("Access-Control-Allow-Origin", "*");
-       response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-       response.setHeader("Access-Control-Max-Age", "3600");
-       response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-       chain.doFilter(req, res);
+      HttpServletResponse response = (HttpServletResponse) res;
+      response.setHeader("Access-Control-Allow-Origin", "*");
+      response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
+      response.setHeader("Access-Control-Max-Age", "3600");
+      response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+      chain.doFilter(req, res);
   }
 
-  @Override
   public void init(FilterConfig filterConfig) {}
 
-  @Override
   public void destroy() {}
 }
