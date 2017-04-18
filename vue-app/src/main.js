9447f309eee037368321174d3050f2dfc9fe3b43
Vitor Manfré <vitormd97@gmail.com>
2017-04-17 21:29:03 -0300

    vue-resource added

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,7 +1,9 @@
 // require(../nodem_modules/bootstrap/dist/bootstrap.min.css);
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
+Vue.use(VueResource);
 
 new Vue({
   el: '#app',
