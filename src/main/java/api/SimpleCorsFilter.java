8565279784aa2df8f6297e71eef4231fd1f18ebd
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-04-18 21:01:31 -0300

    CORS filter updated

--- src/main/java/api/SimpleCorsFilter.java
+++ src/main/java/api/SimpleCorsFilter.java
@@ -19,8 +19,7 @@ public class SimpleCorsFilter implements Filter {
       HttpServletResponse response = (HttpServletResponse) res;
       response.setHeader("Access-Control-Allow-Origin", "*");
       response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH");
-      response.setHeader("Access-Control-Max-Age", "3600");
-      response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+      response.setHeader("Access-Control-Allow-Headers", "Content-Type");
       chain.doFilter(req, res);
   }
 
